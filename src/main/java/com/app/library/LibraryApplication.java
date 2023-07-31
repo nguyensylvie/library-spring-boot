50b859a18b22bcbcc6bb826375a162f9d0a76696
nguyensylvie <57407868+nguyensylvie@users.noreply.github.com>
2023-07-31 17:01:52 +0200

    cache

--- src/main/java/com/app/library/LibraryApplication.java
+++ src/main/java/com/app/library/LibraryApplication.java
@@ -4,8 +4,10 @@ import java.util.Collections;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class LibraryApplication {
 
 	public static void main(String[] args) {
